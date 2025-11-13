bce08f0e8e2886ebfc2cfbd992f79ec8cfe1b2c0
Tanishka Sharma <tanishkasharma@Tanishkas-MacBook-Pro.local>
2025-11-13 12:59:17 -0800

    remove manually import func in cmake

--- src/cpp/calculator.cpp
+++ src/cpp/calculator.cpp
@@ -1,26 +1,23 @@
-#include <iostream>
 #include "calculator.hpp"
-
 using namespace std;
 
-int main() {
-    Calculator calc;
-    double a, b, angle;
-
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-
-    cout << "Addition: " << calc.add(a, b) << endl;
-    cout << "Subtraction: " << calc.subtract(a, b) << endl;
-    cout << "Multiplication: " << calc.multiply(a, b) << endl;
-    cout << "Division: " << calc.divide(a, b) << endl;
-
-    cout << "Enter an angle in degrees: ";
-    cin >> angle;
-
-    cout << "Sine(" << angle << "°): " << calc.sin_deg(angle) << endl;
-    cout << "Cosine(" << angle << "°): " << calc.cos_deg(angle) << endl;
-    cout << "Tangent(" << angle << "°): " << calc.tan_deg(angle) << endl;
-
-    return 0;
-}
+double Calculator::add(double a, double b) 
+             { return a + b; }
+double Calculator::subtract(double a, double b) 
+             { return a - b; }
+double Calculator::multiply(double a, double b) 
+             { return a * b; }
+double Calculator::divide(double a, double b) 
+            { return b != 0 ? a / b : 0; }
+double Calculator::power(double base, double exponent) 
+            { return pow(base, exponent); }
+double Calculator::sin_deg(double degrees) 
+            { return sin(degrees * M_PI / 180.0); }
+double Calculator::cos_deg(double degrees) 
+           { return cos(degrees * M_PI / 180.0); }
+double Calculator::tan_deg(double degrees) 
+          { return std::tan(degrees * M_PI / 180.0); }
+double Calculator::square_root(double value) 
+          { return value >= 0 ? sqrt(value) : 0; }
+double Calculator::logarithm(double value) 
+          { return value > 0 ? log(value) : 0; }
--- src/cpp/calculator.hpp
+++ src/cpp/calculator.hpp
@@ -1,96 +1,19 @@
-#include<cmath>
 
-using namespace std;
+#include <cmath>
+#include <emscripten/emscripten.h>
+#define EXPORT_FUNC EMSCRIPTEN_KEEPALIVE
+
 
 class Calculator {
-
-    public:
-    
-    double add(double a, double b) {
-        return a+b;
-    }
-    double subtract(double a, double b) {
-        return a-b;
-    }
-    double multiply(double a, double b) {
-        return a*b;
-    }
-    double divide(double a, double b) {
-        if(b == 0) {
-            throw "Division by zero error";
-        }
-        return a/b;
-    }
-    double power(double base, double exponent) {
-        return pow(base, exponent);
-    } 
-    double sin_deg(double degrees)
-    {
-        return sin(degrees * M_PI / 180.0);
-    }      
-    double cos_deg(double degrees)
-    {
-        return cos(degrees * M_PI / 180.0);
-    }
-    double tan_deg(double degrees)
-    {
-        return tan(degrees * M_PI / 180.0);
-    }
-    double power(double base, int exponent) {
-        return std::pow(base, exponent);
-    } 
-    double square_root(double value) {
-        if(value < 0) {
-            throw "Square root of negative number error";
-        }
-        return sqrt(value);
-    }
-    double logarithm(double value) {
-        if(value <= 0) {
-            throw "Log of non-positive number error";
-        }
-        return log(value);
-    }
+public:
+    EXPORT_FUNC double add(double a, double b);
+    EXPORT_FUNC double subtract(double a, double b);
+    EXPORT_FUNC double multiply(double a, double b);
+    EXPORT_FUNC double divide(double a, double b);
+    EXPORT_FUNC double power(double base, double exponent);
+    EXPORT_FUNC double sin_deg(double degrees);
+    EXPORT_FUNC double cos_deg(double degrees);
+    EXPORT_FUNC double tan_deg(double degrees);
+    EXPORT_FUNC double square_root(double value);
+    EXPORT_FUNC double logarithm(double value);
 };
-extern "C" {
-    double add(double a, double b) {
-        Calculator c; 
-        return c.add(a, b);
-    }
-    double subtract(double a, double b) {
-        Calculator c; 
-        return c.subtract(a, b);
-    }
-    double multiply(double a, double b) {
-        Calculator c; 
-        return c.multiply(a, b);
-    }
-    double divide(double a, double b) {
-        Calculator c; 
-        return c.divide(a, b);
-    }
-    double power(double a, double b) {
-        Calculator c; 
-        return c.power(a, b);
-    }
-    double sin_deg(double a) {
-        Calculator c; 
-        return c.sin_deg(a);
-    }
-    double cos_deg(double a) {
-        Calculator c; 
-        return c.cos_deg(a);
-    }
-    double tan_deg(double a) {
-        Calculator c; 
-        return c.tan_deg(a);
-    }
-    double square_root(double a) {
-        Calculator c; 
-        return c.square_root(a);
-    }
-    double logarithm(double a) {
-        Calculator c; 
-        return c.logarithm(a);
-    }
-}
