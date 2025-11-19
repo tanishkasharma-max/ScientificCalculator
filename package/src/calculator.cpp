d0a8ae6b6bbf132e2a3b5bf80c26890daa9ab49a
Tanishka Sharma <tanishkasharma@Tanishkas-MacBook-Pro.local>
2025-11-19 12:36:02 -0800

    fix: dynamic mem and remove wasm.cpp

--- package/src/calculator.cpp
+++ package/src/calculator.cpp
@@ -1,5 +1,7 @@
 #include "calculator.hpp"
 #include <stdexcept>
+#include <cmath>
+
 #ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
 #include <cstddef>
@@ -18,60 +20,123 @@ void operator delete(void* ptr, size_t size) noexcept {
 
 using namespace std;
 
-// Calculator logic
-double Calculator::add(const double* a, const double* b) {
-    double* result = new double((*a) + (*b)); 
-    double val = *result;
-    delete result;  
-    return *result;
+double* Calculator::add(const double* a, const double* b) {
+    return new double(*a + *b);
 }
-double Calculator::subtract(const double* a, const double* b) 
-{ 
-    double* result = new double ((*a) - (*b)); 
-     double val = *result;
-    //delete result;  
-    return *result;
+
+double* Calculator::subtract(const double* a, const double* b) {
+    return new double(*a - *b);
 }
-double Calculator::multiply(const double* a, const double* b) 
-{ 
-   double* result = new double ((*a) * (*b)); 
-     double val = *result;
-    //delete result;  
-    return *result;
+
+double* Calculator::multiply(const double* a, const double* b) {
+    return new double(*a * *b);
 }
-double Calculator::divide(const double* a, const double* b) {
+
+double* Calculator::divide(const double* a, const double* b) {
     if (*b == 0) throw runtime_error("Division by zero!");
-    return (*a) / (*b);
+    return new double(*a / *b);
 }
-double Calculator::power(const double* base, const double* exponent) { return pow(*base, *exponent); }
-double Calculator::sin_deg(const double* degrees) { return sin(*degrees * M_PI / 180.0); }
-double Calculator::cos_deg(const double* degrees) { return cos(*degrees * M_PI / 180.0); }
-double Calculator::tan_deg(const double* degrees) { return tan(*degrees * M_PI / 180.0); }
-double Calculator::square_root(const double* value) {
+
+double* Calculator::power(const double* base, const double* exponent) {
+    return new double(pow(*base, *exponent));
+}
+
+double* Calculator::sin_deg(const double* degrees) {
+    return new double(sin(*degrees * M_PI / 180.0));
+}
+
+double* Calculator::cos_deg(const double* degrees) {
+    return new double(cos(*degrees * M_PI / 180.0));
+}
+
+double* Calculator::tan_deg(const double* degrees) {
+    return new double(tan(*degrees * M_PI / 180.0));
+}
+
+double* Calculator::square_root(const double* value) {
     if (*value < 0) throw runtime_error("Cannot take sqrt of negative number!");
-    return sqrt(*value);
+    return new double(sqrt(*value));
 }
-double Calculator::logarithm(const double* value) {
+
+double* Calculator::logarithm(const double* value) {
     if (*value <= 0) throw runtime_error("Log undefined for <= 0!");
-    return log(*value);
+    return new double(log(*value));
 }
 
 #ifdef __EMSCRIPTEN__
 extern "C" {
 
-// Expose all functions to WASM
-EMSCRIPTEN_KEEPALIVE double add(double a, double b) { return Calculator::add(&a, &b); }
-EMSCRIPTEN_KEEPALIVE double subtract(double a, double b) { return Calculator::subtract(&a, &b); }
-EMSCRIPTEN_KEEPALIVE double multiply(double a, double b) { return Calculator::multiply(&a, &b); }
-EMSCRIPTEN_KEEPALIVE double divide(double a, double b) { return Calculator::divide(&a, &b); }
-EMSCRIPTEN_KEEPALIVE double power(double a, double b) { return Calculator::power(&a, &b); }
-EMSCRIPTEN_KEEPALIVE double sin_deg(double v) { return Calculator::sin_deg(&v); }
-EMSCRIPTEN_KEEPALIVE double cos_deg(double v) { return Calculator::cos_deg(&v); }
-EMSCRIPTEN_KEEPALIVE double tan_deg(double v) { return Calculator::tan_deg(&v); }
-EMSCRIPTEN_KEEPALIVE double square_root(double v) { return Calculator::square_root(&v); }
-EMSCRIPTEN_KEEPALIVE double logarithm(double v) { return Calculator::logarithm(&v); }
+// EMSCRIPTEN wrappers: return by value, safe for JS
+EMSCRIPTEN_KEEPALIVE double add(double a, double b) {
+    double* result = Calculator::add(&a, &b);
+    double val = *result;
+    delete result;
+    return val;
+}
 
-// Memory usage tracking
+EMSCRIPTEN_KEEPALIVE double subtract(double a, double b) {
+    double* result = Calculator::subtract(&a, &b);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double multiply(double a, double b) {
+    double* result = Calculator::multiply(&a, &b);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double divide(double a, double b) {
+    double* result = Calculator::divide(&a, &b);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double power(double a, double b) {
+    double* result = Calculator::power(&a, &b);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double sin_deg(double v) {
+    double* result = Calculator::sin_deg(&v);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double cos_deg(double v) {
+    double* result = Calculator::cos_deg(&v);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double tan_deg(double v) {
+    double* result = Calculator::tan_deg(&v);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double square_root(double v) {
+    double* result = Calculator::square_root(&v);
+    double val = *result;
+    delete result;
+    return val;
+}
+
+EMSCRIPTEN_KEEPALIVE double logarithm(double v) {
+    double* result = Calculator::logarithm(&v);
+    double val = *result;
+    delete result;
+    return val;
+}
+
 EMSCRIPTEN_KEEPALIVE size_t getMemoryUsage() { return totalAllocated; }
 
 }
--- package/src/calculator.hpp
+++ package/src/calculator.hpp
@@ -6,16 +6,16 @@
 class Calculator {
 public:
     
-    static double add(const double* a, const double* b);
-    static double subtract(const double* a, const double* b);
-    static double multiply(const double* a, const double* b);
-    static double divide(const double* a, const double* b);
-    static double power(const double* base, const double* exponent);
-    static double sin_deg(const double* degrees);
-    static double cos_deg(const double* degrees);
-    static double tan_deg(const double* degrees);
-    static double square_root(const double* value);
-    static double logarithm(const double* value);
+    static double* add(const double* a, const double* b);
+    static double* subtract(const double* a, const double* b);
+    static double* multiply(const double* a, const double* b);
+    static double* divide(const double* a, const double* b);
+    static double* power(const double* base, const double* exponent);
+    static double* sin_deg(const double* degrees);
+    static double* cos_deg(const double* degrees);
+    static double* tan_deg(const double* degrees);
+    static double* square_root(const double* value);
+    static double* logarithm(const double* value);
 };
 
 
